f4569aaa4a749b3febefe1d07a3e937f8a2dfbb8
rebecsan <rebecca.sandelin@gmail.com>
2019-11-06 22:05:28 +0100

    Eventlisteners and functions to get dates from form relates #11

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
       let json = await response.json();
       console.log(json);
       renderMeasureSites(json);
+      // Get dates entered in search field
+      document.getElementById('fromDate').addEventListener('input', getFromDate);
+      document.getElementById('toDate').addEventListener('input', getToDate);
     })
 })
 
@@ -37,6 +40,19 @@ let getMeasureParameter = (measureParameters, code) => {
   }
 }
 // Get input information from form
+// Dates:
+let fromDate;
+let toDate;
 
+function getFromDate(event) {
+  fromDate = event.target.value;
+  console.log(fromDate);
+  event.preventDefault();
+}
+function getToDate(event) {
+  toDate = event.target.value;
+  console.log(toDate);
+  event.preventDefault();
+}
 // Max value (to) should be yesterday
 // Default from value is one month ago
