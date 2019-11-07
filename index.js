c3cc8da5f84c6f7aac4d0531c6c3a5d180dfd0b8
AnionaStella <eiljon88@gmail.com>
2019-11-07 10:43:21 +0100

    added try..catch to fetch, className to flowValue

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://data.goteborg.se/RiverService/v1.1/MeasureSites/b9098f14-4d94-49bd-8c7b-2c15ab9c370e?format=json')
-    .then(async response => {
-      let json = await response.json();
-      console.log(json);
-      renderMeasureSites(json);
-      renderformName(json);
-      // Get dates entered in search field
-      document.getElementById('fromDate').addEventListener('input', getFromDate);
-      document.getElementById('toDate').addEventListener('input', getToDate);
-    })
+  try {
+    fetch('http://data.goteborg.se/RiverService/v1.1/MeasureSites/b9098f14-4d94-49bd-8c7b-2c15ab9c370e?format=json')
+      .then(async response => {
+        let json = await response.json();
+        console.log(json);
+        renderMeasureSites(json);
+        renderformName(json);
+        // Get dates entered in search field
+        document.getElementById('fromDate').addEventListener('input', getFromDate);
+        document.getElementById('toDate').addEventListener('input', getToDate);
+      })
+  } catch (error) {
+    console.error(error);
+  }
 })
 
 
@@ -25,6 +29,7 @@ let renderMeasureSites = measureSites => {
     siteCard.appendChild(siteNameParagraph);
     // create <p> for  flow content
     let siteFlowParagraph = document.createElement("p");
+    siteFlowParagraph.className = "flowValue";
     let measureParameter = getMeasureParameter(
       measureSite.MeasureParameters,
       "Tapping" || "Flow"
